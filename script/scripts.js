42fa64a0f32684357047388941c4e1b2065ee63b
Bruno Tenório <tenoriobn@gmail.com>
2023-04-21 12:04:51 -0300

    Tornando dados dinâmicos e atualizados no gráfico

--- script/scripts.js
+++ script/scripts.js
@@ -3,10 +3,10 @@ const graficoDolar = document.getElementById('graficoDolar');
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Dólar',
+            data: [],
             borderWidth: 1
         }]
     },
@@ -17,7 +17,10 @@ setInterval(() => conectaAPI(), 5000);
 async function conectaAPI() {
     const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
     const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
+    
+    let tempo = geraHorario();
+    let valor = conectaTraduzido.USDBRL.ask;
+    adicionarDados(graficoParaDolar, tempo, valor);
 }
 
 function geraHorario() {
@@ -27,4 +30,10 @@ function geraHorario() {
     return horario;
 }
 
-geraHorario();
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda);
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados);
+    });
+    grafico.update();
+}
