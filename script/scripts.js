2408a611405cc393154b1d511360e174f7bd0bfd
Bruno Tenório <tenoriobn@gmail.com>
2023-04-28 18:33:50 -0300

    Criando gráfico de cotação da moeda Iene

--- script/scripts.js
+++ script/scripts.js
@@ -37,3 +37,27 @@ workerDolar.addEventListener("message", event => {
     imprimeCotacao("dolar", valor);
     adicionarDados(graficoParaDolar, tempo, valor);
 })
+
+const graficoIene = document.getElementById('graficoIene');
+
+const graficoParaIene = new Chart(graficoIene, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Iene',
+            data: [],
+            borderWidth: 1
+        }]
+    },
+});
+
+let workerIene = new Worker('./script/workers/workerIene.js');
+workerIene.postMessage('iene');
+
+workerIene.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    imprimeCotacao("iene", valor);
+    adicionarDados(graficoParaIene, tempo, valor);
+})
